aea9e3ecaee0bb5ce1529e9320bb2ddb256e78b1
kaike909 <caiquematano96@gmail.com>
2022-07-28 20:47:19 -0300

    Changed Stuff

--- JavaVsCode/src/Fundamentos/Temperatura.java
+++ JavaVsCode/src/Fundamentos/Temperatura.java
@@ -6,10 +6,10 @@ public class Temperatura {
     public static void main(String[] args) {
         // (F - 32) x 5/9 = C
         Scanner in = new Scanner(System.in);
-        System.out.print("Digite a temperatura em Fahrenheit = ");
-        int f = in.nextInt();
         final int a = 32;
         final double b = 5 / 9.0;
+        System.out.print("Digite a temperatura em Fahrenheit = ");
+        int f = in.nextInt();
         double c = (f - a) * b;
         System.out.printf("A conversao de %dF, para C fica = %.1fC", f, c);
         in.close();

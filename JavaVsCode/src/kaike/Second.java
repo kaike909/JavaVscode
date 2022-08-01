39260ab2299b894040c3ed62bb704946883e6f11
kaike909 <caiquematano96@gmail.com>
2022-08-01 20:26:00 -0300

    Added and Changed files

---
+++ JavaVsCode/src/kaike/Second.java
@@ -0,0 +1,25 @@
+package kaike;
+
+import java.util.Scanner;
+
+public class Second {
+
+    public static void sayName(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = in.nextLine();
+        System.out.print("Digite o seu sobrenome: ");
+        String sobrenome = in.nextLine();
+        System.out.printf("%s %s e tem %d anos.", nome, sobrenome, sayAge());
+        in.close();
+    }
+
+    public static int sayAge(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Digiet a sua idade: ");
+        int age = in.nextInt();
+        in.nextLine();
+        in.close();
+        return age;
+    }
+}

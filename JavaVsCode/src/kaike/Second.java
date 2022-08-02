e9ddd30b14a448e1ec6afc5412fd91b5006acb49
kaike909 <caiquematano96@gmail.com>
2022-08-01 21:36:12 -0300

    Added Files

--- JavaVsCode/src/kaike/Second.java
+++ JavaVsCode/src/kaike/Second.java
@@ -3,23 +3,26 @@ package kaike;
 import java.util.*;
 
 public class Second {
-
+    public static void main(String[] args) {
+        sayAge();
+        sayName();
+    }
+    
     public static void sayName(){
         Scanner in = new Scanner(System.in);
         System.out.print("Digite o seu nome: ");
         String nome = in.nextLine();
         System.out.print("Digite o seu sobrenome: ");
         String sobrenome = in.nextLine();
-        System.out.printf("%s %s e tem %d anos.", nome, sobrenome, sayAge());
+        System.out.printf("%s %s tem %d anos.", nome, sobrenome, sayAge());
         in.close();
     }
 
     public static int sayAge(){
         Scanner in = new Scanner(System.in);
-        System.out.print("Digiet a sua idade: ");
         int age = in.nextInt();
-        in.nextLine();
         in.close();
         return age;
     }
+
 }

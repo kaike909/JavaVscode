67adc670db883f698e31509469a7e107ab3985f4
kaike909 <caiquematano96@gmail.com>
2022-08-01 21:46:28 -0300

    Added Files

--- JavaVsCode/src/Fundamentos/ConversaoPrimitivos.java
+++ JavaVsCode/src/Fundamentos/ConversaoPrimitivos.java
@@ -10,8 +10,12 @@ public class ConversaoPrimitivos {
         System.out.println(b);
 
         int c = 130;
-        byte d = (byte) c;
+        byte d = (byte) c; // Explicita (CAST)
         System.out.println(d);
+
+        double e = 1.0;
+        int f = (int) e;
+        System.out.println(f);
 
     }
 }

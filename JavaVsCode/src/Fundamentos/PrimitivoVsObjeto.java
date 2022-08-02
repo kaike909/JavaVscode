d5c980e239989b8c69450d3116485f18ed3afd3c
kaike909 <caiquematano96@gmail.com>
2022-08-01 21:18:07 -0300

    Added Files

--- JavaVsCode/src/Fundamentos/PrimitivoVsObjeto.java
+++ JavaVsCode/src/Fundamentos/PrimitivoVsObjeto.java
@@ -3,7 +3,7 @@ package Fundamentos;
 public class PrimitivoVsObjeto {
     public static void main(String[] args) {
         String s = "texto";
-        s.toUpperCase();
+        s = s.toUpperCase();
 
         //Wrappers sao a versao objeto dos tipos primitivos!
         int a = 123;

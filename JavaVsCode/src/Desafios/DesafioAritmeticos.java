e20fbfa4752dcd06d8fe83aa1977ed8b9d3de6b9
kaike909 <caiquematano96@gmail.com>
2022-08-03 12:58:38 -0300

    Added new Files

--- JavaVsCode/src/Desafios/DesafioAritmeticos.java
+++ JavaVsCode/src/Desafios/DesafioAritmeticos.java
@@ -2,13 +2,16 @@ package Desafios;
 
 public class DesafioAritmeticos {
     public static void main(String[] args) {
+
         int a = 6 * (3 + 2);
         int b = (int) Math.pow(a, 2);
         int c = 3 * 2;
         int d = b / c;
+
         int e = (1 - 5) * (2 - 7) / 2;
         int f = (int) Math.pow(e, 2);
         int g = d - f;
+
         int h = (int) Math.pow(g, 3) / (int) Math.pow(10, 3);
 
         System.out.println(b);
@@ -16,5 +19,6 @@ public class DesafioAritmeticos {
         System.out.println(f);
         System.out.println(g);
         System.out.println(h);
+        
     }
 }

979831c7422b781be03630e7b17a3fe8ae83c8c7
kaike909 <caiquematano96@gmail.com>
2022-08-05 17:37:59 -0300

    Added new Files

--- JavaVsCode/src/Classe/ProdutoTeste.java
+++ JavaVsCode/src/Classe/ProdutoTeste.java
@@ -3,15 +3,11 @@ package Classe;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto();
-        p1.nome = "Notebook";
-        p1.preco = 4356.89;
-        Produto.desconto = 0.15;
+        Produto p1 = new Produto("NoteBook", 4356.89);
 
         var p2 = new Produto();
         p2.nome = "Caneta Preta";
         p2.preco = 12.56;
-        Produto.desconto = 0.3;
 
         System.out.println(p1.nome);
         double precoFinal1 = p1.precoComDesconto(0.45);

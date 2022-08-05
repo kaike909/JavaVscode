6e722613d9044f1c908dcf2df75d62808450ac83
kaike909 <caiquematano96@gmail.com>
2022-08-05 12:43:27 -0300

    Added new Files

--- JavaVsCode/src/Classe/Produto.java
+++ JavaVsCode/src/Classe/Produto.java
@@ -5,4 +5,14 @@ public class Produto {
     String nome;
     double preco, desconto;
 
+    static double precoComDesconto(double a, double b){
+        double desc = a * (1 - b);
+        return desc;
+    }
+
+    static double media(double a, double b){
+        double media = (a + b) / 2;
+        return media;
+    }
+
 }

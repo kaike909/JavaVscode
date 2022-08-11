e22dbaeff96c192727c21ba62d0195f9d19c8acc
kaike909 <caiquematano96@gmail.com>
2022-08-11 15:01:20 -0300

    Added new Files

--- JavaVsCode/src/OOP/Composicao/CompraTeste.java
+++ JavaVsCode/src/OOP/Composicao/CompraTeste.java
@@ -5,11 +5,12 @@ public class CompraTeste {
 
         Compra c1 = new Compra();
         c1.cliente = "Joao Pedro";
-        c1.itens.add(new Item("Caneta", 20, 7.45));
+        c1.adcItem("Caneta", 20, 7.45);;
         c1.itens.add(new Item("Borracha", 12, 3.89));
         c1.itens.add(new Item("Caderno", 3, 18.79));
 
         System.out.println(c1.itens.size());
+        System.out.println(c1.getTotal());
 
     }
 }

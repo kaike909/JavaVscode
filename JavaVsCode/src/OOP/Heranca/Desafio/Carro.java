3e1ec9772ff80fe7e75bf9663be165ed2133aa5d
kaike909 <caiquematano96@gmail.com>
2022-08-17 20:16:08 -0300

    Added new Files

--- JavaVsCode/src/OOP/Heranca/Desafio/Carro.java
+++ JavaVsCode/src/OOP/Heranca/Desafio/Carro.java
@@ -2,6 +2,18 @@ package OOP.Heranca.Desafio;
 
 public class Carro {
     
-    
+    int velocidadeAtual;
+
+    void acelerar(){
+        velocidadeAtual += 5;
+    }
+
+    void frear() {
+        if (velocidadeAtual >= 5) {
+            velocidadeAtual -= 5;
+        } else {
+            velocidadeAtual = 0;
+        }
+    }
 
 }

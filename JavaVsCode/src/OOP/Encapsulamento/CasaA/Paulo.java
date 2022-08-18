d103769e09247af694b71776cf01d92354d895a1
kaike909 <caiquematano96@gmail.com>
2022-08-18 18:17:24 -0300

    Added new Files

--- JavaVsCode/src/OOP/Encapsulamento/CasaA/Paulo.java
+++ JavaVsCode/src/OOP/Encapsulamento/CasaA/Paulo.java
@@ -5,15 +5,12 @@ public class Paulo {
     Ana esposa = new Ana();
     
     void testeAcesso() {
-        // segredo
-        // facoDentroDeCasa
-        // formaDeFalar
-        // todosSabem
-        
+
         // System.out.println(esposa.segredo);
         System.out.println(esposa.facoDentroDeCasa);
         System.out.println(esposa.formaDeFalar);
         System.out.println(esposa.todosSabem);
+
     }
 
 }
--- JavaVsCode/src/OOP/Encapsulamento/CasaB/Pedro.java
+++ JavaVsCode/src/OOP/Encapsulamento/CasaB/Pedro.java
@@ -5,16 +5,12 @@ import OOP.Encapsulamento.CasaA.Ana;
 public class Pedro extends Ana {
     
     void testeAcesso() {
-        // segredo
-        // facoDentroDeCasa
-        // formaDeFalar
-        // todosSabem
-        
         // System.out.println(segredo);
         // System.out.println(facoDentroDeCasa);
         System.out.println(formaDeFalar);
         System.out.println(todosSabem);
         System.out.println(new Ana().todosSabem);
+        
     }
 
 }

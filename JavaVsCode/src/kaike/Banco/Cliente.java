39c931afda3175a3b16538f52cafea13d5d91ec9
kaike909 <caiquematano96@gmail.com>
2022-08-19 11:12:56 -0300

    Added new Files

---
+++ JavaVsCode/src/kaike/Banco/Cliente.java
@@ -0,0 +1,9 @@
+package kaike.Banco;
+
+public class Cliente extends Banco {
+
+    Cliente(String nome, long cpf, int conta, double dinheiro) {
+        super(nome, cpf, conta, dinheiro);
+    }
+
+}

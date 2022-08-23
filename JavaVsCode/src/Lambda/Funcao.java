4bfafc94b2de904df4dc6afd3d68bed9a7ed742b
kaike909 <caiquematano96@gmail.com>
2022-08-23 18:40:07 -0300

    Added Files

--- JavaVsCode/src/Lambda/Funcao.java
+++ JavaVsCode/src/Lambda/Funcao.java
@@ -8,14 +8,24 @@ public class Funcao {
 
         Function<Integer, String> parOuImpar = numero -> numero % 2 == 0 ? "Par" : "Impar";
 
-        System.out.println(parOuImpar.apply(32));
-
-        Function<String, String> oResultadoE = valor -> "O resultado é " + valor;
+        Function<String, String> oResultadoE = valor -> "O resultado é: " + valor;
 
         Function<String, String> empolgado = valor -> valor + "!!!";
 
-        String resultadoFinal = parOuImpar.andThen(oResultadoE).andThen(empolgado).apply(32);
-        System.out.println(resultadoFinal);
+        Function<String, String> duvida = valor -> valor + "???";
+
+        String resultadoFinal1 = parOuImpar
+                .andThen(oResultadoE)
+                .andThen(empolgado)
+                .apply(32);
+        String resultadoFinal2 = parOuImpar
+                .andThen(oResultadoE)
+                .andThen(duvida)
+                .apply(33);
+
+        System.out.println(parOuImpar.apply(32));
+        System.out.println(resultadoFinal1);
+        System.out.println(resultadoFinal2);
 
     }
 

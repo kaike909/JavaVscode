ecdf0d81702c9b696aa5c2e594ef2aba09628527
kaike909 <caiquematano96@gmail.com>
2022-08-23 21:31:15 -0300

    Added Files

--- JavaVsCode/src/Lambda/Predicado.java
+++ JavaVsCode/src/Lambda/Predicado.java
@@ -5,10 +5,10 @@ import java.util.function.Predicate;
 public class Predicado {
     public static void main(String[] args) {
 
-        Predicate<Produto> isCaro = prod -> (prod.preco * (1-prod.desconto)) >= 750;
+        Predicate<Produto> isCaro = prod -> (prod.preco * (1 - prod.desconto)) >= 750;
 
-        Produto produto = new Produto("Notebooke", 3893.89, 0.15);
+        Produto produto = new Produto("Notebook", 3893.89, 0.15);
         System.out.println(isCaro.test(produto));
 
-    }   
+    }
 }

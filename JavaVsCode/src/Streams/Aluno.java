55784a7dde0a9331bba69aea6c8fc246f13fc1b3
kaike909 <caiquematano96@gmail.com>
2022-08-25 18:57:30 -0300

    Added new Files

--- JavaVsCode/src/Streams/Aluno.java
+++ JavaVsCode/src/Streams/Aluno.java
@@ -3,11 +3,17 @@ package Streams;
 public class Aluno {
 
     final String nome;
-    final Double nota;
+    final double nota;
+    final boolean bomComp;
 
     public Aluno(String nome, double nota) {
+        this(nome, nota, true);
+    }
+
+    public Aluno(String nome, double nota, boolean bomComp) {
         this.nome = nome;
         this.nota = nota;
+        this.bomComp = bomComp;
     }
 
 }

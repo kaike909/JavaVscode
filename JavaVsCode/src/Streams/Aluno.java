c805a3e75d08ac1ac887fcb568641b7a8f6b4d76
kaike909 <caiquematano96@gmail.com>
2022-08-26 16:55:49 -0300

    Added Files

--- JavaVsCode/src/Streams/Aluno.java
+++ JavaVsCode/src/Streams/Aluno.java
@@ -16,4 +16,44 @@ public class Aluno {
         this.bomComp = bomComp;
     }
 
+    public String toString() {
+        return nome + " tem nota " + nota;
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Aluno other = (Aluno) obj;
+        if (bomComp != other.bomComp) {
+            return false;
+        }
+        if (nome == null) {
+            if (other.nome != null) {
+                return false;
+            } else if (!nome.equals(other.nome)) {
+                return false;
+            }
+            if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
 }

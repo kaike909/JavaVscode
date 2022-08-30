1b4f73ef8b0cb91453cdbe80b18b7eb743afa429
kaike909 <caiquematano96@gmail.com>
2022-08-30 14:22:21 -0300

    Added Files

--- JavaVsCode/src/Excecao/Aluno.java
+++ JavaVsCode/src/Excecao/Aluno.java
@@ -2,9 +2,9 @@ package Excecao;
 
 public class Aluno {
 
-    final String nome;
-    final double nota;
-    final boolean bomComp;
+    public final String nome;
+    public final double nota;
+    public final boolean bomComp;
 
     public Aluno(String nome, double nota) {
         this(nome, nota, true);
---
+++ JavaVsCode/src/Excecao/personalizadaA/NumeroForaIntervaloException.java
@@ -0,0 +1,15 @@
+package Excecao.personalizadaA;
+
+public class NumeroForaIntervaloException extends RuntimeException {
+
+    private String nomeDoAtributo;
+
+    public NumeroForaIntervaloException(String nomeDoAtributo) {
+        this.nomeDoAtributo = nomeDoAtributo;
+    }
+
+    public String getMessage() {
+        return String.format("O atributo '%s' esta fora do intervalo", nomeDoAtributo);
+    }
+
+}

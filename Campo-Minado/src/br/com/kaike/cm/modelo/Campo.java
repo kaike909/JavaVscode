081f8ddf2c71f17889f951f0bd2c2220bd51e8c9
kaike909 <caiquematano96@gmail.com>
2022-08-30 23:01:19 -0300

    Added Files

---
+++ Campo-Minado/src/br/com/kaike/cm/modelo/Campo.java
@@ -0,0 +1,46 @@
+package br.com.kaike.cm.modelo;
+
+import java.util.*;
+
+public class Campo {
+
+    private final int lin;
+    private final int col;
+
+    private boolean open;
+    private boolean mined;
+    private boolean marked;
+
+    private List<Campo> neighbors = new ArrayList<>();
+
+    public Campo(int lin, int col, boolean mined) {
+        this.lin = lin;
+        this.col = col;
+        this.mined = mined;
+    }
+
+    public Campo(int lin, int col) {
+        this.lin = lin;
+        this.col = col;
+    }
+
+    public boolean addNeighbor(Campo neighbor) {
+        boolean difLin = lin != neighbor.lin;
+        boolean difcol = col != neighbor.col;
+        boolean diagonal = difLin && difcol;
+
+        int deltaLin = Math.abs(lin - neighbor.lin);
+        int deltaCol = Math.abs(col - neighbor.col);
+        int deltaGer = deltaCol + deltaLin;
+
+        if (deltaGer == 1 && !diagonal) {
+            neighbors.add(neighbor);
+            return true;
+        } else if (deltaGer == 2 && diagonal) {
+            neighbors.add(neighbor);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ Campo-Minado/src/br/com/kaike/cm/modelo/Tabuleiro.java
@@ -0,0 +1,5 @@
+package br.com.kaike.cm.modelo;
+
+public class Tabuleiro {
+
+}

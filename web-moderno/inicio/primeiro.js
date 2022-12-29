5b66a774692fcee0e9e1c094aed2448d03fd49a9
Carlos Henrique Matano <caiquematano96@gmail.com>
2022-12-29 11:14:05 -0300

    Added Files

--- web-moderno/inicio/primeiro.js
+++ web-moderno/inicio/primeiro.js
@@ -11,3 +11,13 @@ console.log(media.toFixed(2))
 
 let escola = 'cod3r';
 console.log(escola.charAt(2))
+
+ let nomes = 'Ana, Bianca, Carla, Debora'
+console.log(nomes.split(','))
+ for(let i = 0; i < nomes.length; i++){
+    if(nomes.charAt(i) === ',' || nomes.charAt(i) === ' '){
+        continue
+    } else {
+        console.log(nomes.charAt(i))
+    }
+ }

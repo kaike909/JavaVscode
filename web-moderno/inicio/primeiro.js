762dd8c9f4c11c25d68a74e41e90538f21f34d5c
Carlos Henrique Matano <caiquematano96@gmail.com>
2022-12-29 00:27:36 -0300

    Added Files

--- web-moderno/inicio/primeiro.js
+++ web-moderno/inicio/primeiro.js
@@ -1,3 +1,10 @@
 console.log("Legal!!!")
 let nome = "Carlos Henrique Matanó"
 console.log(`Meu nome é ${nome}`)
+
+let nota1 = 8.234;
+let nota2 = 5.789;
+
+var media = (nota1 + nota2) / 2
+
+console.log(media.toFixed(2))

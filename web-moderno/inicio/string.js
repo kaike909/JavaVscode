ef80183862187a4669fcc482cc5772c24fd4d283
Carlos Henrique Matano <caiquematano96@gmail.com>
2022-12-30 11:18:09 -0300

    Added Files

--- web-moderno/inicio/string.js
+++ web-moderno/inicio/string.js
@@ -26,3 +26,28 @@ let nome1 = 'Carlos'
 let nome2 = 'Carlos'
 
 console.log(nome1 == nome2)
+
+const up = s => s.toUpperCase();
+
+console.log(up("cuidado"))
+
+let isAtivo = 1;
+
+console.log(!isAtivo)
+
+console.log(`O valor de !!3 é ${!!3}`)
+console.log(`O valor de !!' ' é ${!!' '}`)
+console.log(`O valor de !!'texto' é ${!!'texto'}`)
+console.log(`O valor de !![] é ${!![]}`)
+console.log(`O valor de !!{} é ${!!{}}`)
+console.log(`O valor de !!infinity é ${!!Infinity}`)
+console.log(`O valor de !!isAtivo = true é ${!!(isAtivo = true)}`)
+
+console.log(`O valor de !!0 é ${!!0}`)
+console.log(`O valor de !!'' é ${!!''}`)
+console.log(`O valor de !!null é ${!!null}`)
+console.log(`O valor de !!NaN é ${!!NaN}`)
+console.log(`O valor de !!undefined é ${!!undefined}`)
+console.log(`O valor de !!isAtivo = true é ${!!(isAtivo = false)}`)
+
+console.log('' || null || undefined || 'opa' || 123)

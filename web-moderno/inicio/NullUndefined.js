f8a57217cfa57046574198b757b1f70d4fe56bc6
Carlos Henrique Matano <caiquematano96@gmail.com>
2023-01-04 17:38:50 -0300

    Added files

---
+++ web-moderno/inicio/NullUndefined.js
@@ -0,0 +1,6 @@
+const a = {nome: 'Teste'}
+const b = a
+console.log(a.nome)
+console.log(b.nome)
+b.nome = 'Opa'
+console.log(b.nome)
--- web-moderno/inicio/objeto.js
+++ web-moderno/inicio/objeto.js
@@ -6,9 +6,23 @@ prod1['Besconto Legal'] = 0.40
 console.log(`O ${prod1.nome} está custando R$ ${prod1.preco}`);
 console.log(prod1)
 
+const prod2 = {
+    nome: 'Notebook',
+    preco: 3499.90,
+    obj:{
+        blabla: 1,
+        obj:{
+            blabla: 2
+        }
+    }
+}
+
+console.log(prod2)
+
+//array e concat
 var arr1 = ['a', 'b', 'c'];
 var arr2 = ['d', 'e', 'f'];
 var arr3 = ['g', 'h', 'i'];
 
 const result = arr1.concat(arr2, arr3)
-console.log(result)
+console.log(`Todas as variáveis dão o resultado ${result}`)

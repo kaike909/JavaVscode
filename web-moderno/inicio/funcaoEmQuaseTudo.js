2df4ccb7d64e9f6336557ab5663f7479372b4913
Carlos Henrique Matano <caiquematano96@gmail.com>
2023-01-05 12:08:00 -0300

    added files

--- web-moderno/inicio/funcaoEmQuaseTudo.js
+++ web-moderno/inicio/funcaoEmQuaseTudo.js
@@ -1,24 +1,52 @@
+function pontinhos(ceu) {
+    console.log(`${'-'.repeat(10)} ${ceu} ${'-'.repeat(10)}`)
+}
 console.log(typeof Object)
 
 class Produto {}
 
 console.log(typeof Produto)
 
-console.log("------ Funcao 1! ------")
-console.log("- Função sem retorno! -")
-
+pontinhos("Função 1!")
+pontinhos("Função sem retorno!")
 function imprimirSoma(a, b){
     console.log(`A soma de ${a} + ${b} é igual a ${a + b}`)
 }
 
 imprimirSoma(2, 2)
 
-console.log("- Função com retorno! -")
+pontinhos("Função com retorno!")
 
-function soma(a = 0, b = 0){
+function soma(a = null, b = null){
     return a + b
 }
 
 console.log(soma(2, 3))
 console.log(soma(2))
 console.log(soma())
+
+pontinhos("Função 2!")
+pontinhos("Armazenando função em variável!")
+
+const imprimeSoma = function(a, b){
+    console.log(a + b)
+}
+
+imprimeSoma(1, 2)
+
+pontinhos("Armazenando uma função arrow numa variável!")
+
+const some = (a, b) => {
+    return a + b
+}
+
+console.log(some(2, 3))
+
+pontinhos("Retorno implícito")
+
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(3, 2))
+
+const imprimir2 = a => console.log(`${'-'.repeat(3)} ${a} ${'-'.repeat(3)}`)
+imprimir2("Quero trabalhar logo na área!")

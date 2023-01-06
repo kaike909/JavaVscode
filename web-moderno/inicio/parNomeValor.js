5e06c6f3abfdc8a6abc4a8dbd3a4c1408362c691
Carlos Henrique Matano <caiquematano96@gmail.com>
2023-01-06 00:44:56 -0300

    added files

--- web-moderno/inicio/parNomeValor.js
+++ web-moderno/inicio/parNomeValor.js
@@ -2,3 +2,24 @@ const pontinhos = a => console.log(`${'-'.repeat(10)} ${a} ${'-'.repeat(10)}`)
 pontinhos('par Nome/Valor!')
 
 const saudacao = 'Ola' // contexto léxico 1
+
+function exec(){
+    const saudacao = 'Falaaa' // contexto léxico 2
+    return saudacao
+}
+
+// Objetos são grupos aninhados de pares nome/valor
+const cliente = {
+    nome: 'Almiro Souza',
+    idade: 26,
+    peso: 75,
+    endereco: {
+        logradouro: 'Rua Adomilro Alves Pereira',
+        numero: 48,
+        bairro: 'Jardim Euvira magalhães'
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(`Meu nome é ${cliente.nome}, tenho ${cliente.idade}, peso ${cliente.peso}, moro na ${cliente.endereco.logradouro}, numero ${cliente.endereco.numero}, no bairro ${cliente.endereco.bairro}`)

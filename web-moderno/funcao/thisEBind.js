eb7c0fea125fa3712d1517bc8518fe52872850fd
Carlos Henrique Matano <caiquematano96@gmail.com>
2023-01-08 12:30:36 -0300

    added files

---
+++ web-moderno/funcao/thisEBind.js
@@ -0,0 +1,15 @@
+const pessoa = {
+    saudacao: 'Bom Dia!',
+    falar(){
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+
+const falar = pessoa.falar
+
+falar() // conflito entre programação funcional e OO
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()

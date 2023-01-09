40ee23c87adb077c653c1a72649d78b924b10ec5
Carlos Henrique Matano <caiquematano96@gmail.com>
2023-01-08 23:15:57 -0300

    added files

--- web-moderno/gitHub/linkedList.js
+++ web-moderno/gitHub/linkedList.js
@@ -4,45 +4,36 @@ class Node {
         this.next = next;
     }
 }
-
 class LinkedList {
     constructor(){
         this.head = null;
         this.size = 0;
     }
-
     // inserir primeiro nó
     insertFisrt(data){
         this.head = new Node(data, this.head);
         this.size++;
     }
-
     // inserir ultimo nó
     insertLast(data){
         let node = new Node(data);
         let current;
-
         if(!this.head){
             this.head = node;
         } else {
             current = this.head;
-
             while(current.next){
                 current = current.next;
             }
-
             current.next = node;
         }
-
         this.size++;
     }
-
     // inserir no index
     insertAt(data, index){
         if(index > 0 && index > this.size){
             return;
         }
-
         if(index === 0){
             this.insertFisrt(data);
             // this.head = new Node(data, this.head);
@@ -50,10 +41,8 @@ class LinkedList {
         }
         const node = new Node(data);
         let current, previous
-
         current = this.head;
         let count = 0;
-
         while(count < index){
             previous = current;
             count++;
@@ -61,10 +50,8 @@ class LinkedList {
         }
         node.next = current;
         previous.next = node;
-
         this.size++;
     }
-
     // Remover no Index
     removeAt(index){
         if(index > 0 && index > this.size){
@@ -73,7 +60,6 @@ class LinkedList {
         let current = this.head;
         let previous;
         let count = 0;
-
         if(index === 0){
             this.head = current.next;
         } else {
@@ -86,12 +72,10 @@ class LinkedList {
         }
         this.size--;
     }
-
     // Get no Index
     getAt(index){
         let current = this.head;
         let count = 0;
-
         while(current){
             if(count == index){
                 console.log(`O valor ${current.data} esta no index ${index}`);
@@ -101,34 +85,26 @@ class LinkedList {
         }
         return null;
     }
-
     // limpar lista
     clearList(){
         this.head = null;
         this.size = 0;
     }
-
     // imprimir em lista
     printListData(){
         let current = this.head;
-
         while(current){
             console.log(current.data)
             current = current.next
         }
     }
-
 }
-
 const ll = new LinkedList();
-
 ll.insertFisrt(100)
 ll.insertLast(200)
 ll.insertLast(300)
 ll.insertLast(400)
 ll.insertAt(500, 2)
-
 ll.clearList()
-
 ll.printListData()
 // ll.getAt(2)

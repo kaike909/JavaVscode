c9c4fdf2866777128c5ee51c48d1c6d7a2e93bb4
Carlos Henrique Matano <caiquematano96@gmail.com>
2023-01-10 02:11:05 -0300

    added files

--- web-moderno/funcao/callback.js
+++ web-moderno/funcao/callback.js
@@ -1,8 +1,44 @@
+const pontinhos = param => console.log(`${'-'.repeat(15)} ${param} ${'-'.repeat(15)}`)
+pontinhos('Callback 1!')
+
 const fabricantes = ['Mercedes', 'Audi', 'BMW']
 
-function imprimir(nome, index){
+function imprimir(nome, index) {
     console.log(`${index + 1}.${nome}`)
 }
 
 fabricantes.forEach(imprimir)
 fabricantes.forEach(a => console.log(`${a}`))
+
+pontinhos('Callback 2!')
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+let notasBaixas = []
+
+for (let i in notas) {
+    if (notas[i] < 7) {
+        notasBaixas.push(notas[i])
+    }
+}
+
+let notasAltas = []
+
+for (let i in notas) {
+    if (notas[i] >= 7){
+        notasAltas.push(notas[i])
+    }
+}
+
+notasBaixas = notas.filter( n => n < 7)
+notasAltas = notas.filter(n => n >= 7)
+
+console.log(notasBaixas)
+console.log(notasAltas)
+
+const notasMenorQue7 = n => n < 7
+const notasMaiorQue7 = n => n >= 7
+const notasBaixas2 = notas.filter(notasMenorQue7)
+const notasAltas2 = notas.filter(notasMaiorQue7)
+
+console.log(notasBaixas2)
+console.log(notasAltas2)

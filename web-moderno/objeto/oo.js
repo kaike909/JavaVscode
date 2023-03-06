3042eb08c7a8c0560aae5a9cf5dd077246e3329d
kaike909 <caiquematano96@gmail.com>
2023-03-06 20:34:35 -0300

    Added Files

---
+++ web-moderno/objeto/oo.js
@@ -0,0 +1,2 @@
+// Codigo nao executavel!!
+
--- web-moderno/training.js
+++ web-moderno/training.js
@@ -1,3 +1,5 @@
-const aleatorio = (min = 10000000, max = 99999999) => `9${Math.floor(Math.random() * (max - min) + min)}`
+const nth = (a, b, c, d, e, f) => (((a + b - c) * d) / e) ** f
 
-console.log(aleatorio())
+console.log(nth(6,5,4,3,2,1))
+console.log(nth(6,2,1,4,2,3))
+console.log(nth(2,3,6,4,2,3))
